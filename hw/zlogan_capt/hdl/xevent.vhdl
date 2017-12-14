e2bb2985923b0e14b1073cd1f151516ff5cb11aa
Pavel Pisa <pisa@cmp.felk.cvut.cz>
2017-12-14 21:01:33 +0100

    Commit more Marek Peca's original zlogan files.

---
+++ hw/zlogan_capt/hdl/xevent.vhdl
@@ -0,0 +1,45 @@
+--
+-- resynchronization of external asynchronous event
+-- and its justification to span over exactly one target clok period
+-- to use e.g. for CPU bus vs. external clock core signalling
+-- ("data ready" etc.)
+--
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity xevent is
+  generic (
+    n_resync: natural := 2  -- n of resync DFFs to reach given MTBF 
+  );
+  port (
+    trig: in std_logic;
+    clock: in std_logic;
+    q: out std_logic
+  );
+end xevent;
+
+architecture rtl of xevent is
+  signal x: std_logic;
+  signal y: std_logic_vector (n_resync+1 downto 0);
+begin
+  y(y'high) <= x;
+  q <= y(1) and not y(0);
+  
+  seq: process
+  begin
+    wait until clock'event and clock = '1';
+    y(y'high-1 downto 0) <= y(y'high downto 1);
+  end process;
+
+  set: process (trig, clock)
+  begin
+    if trig = '1' then
+      x <= '1';
+    elsif clock'event and clock = '1' then
+      x <= x and not y(1);
+    end if;
+  end process;
+end rtl;
+
---
+++ hw/zlogan_capt/hdl/zlogan_capt_rxdma.vhdl
@@ -0,0 +1,161 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity zlogan_capt_rxdma is
+  generic (
+    -- Users to add parameters here
+
+    -- User parameters ends
+    -- Do not modify the parameters beyond this line
+
+    -- Width of S_AXIS address bus. The slave accepts the read and write addresses of width C_M_AXIS_TDATA_WIDTH.
+    S2MM_TDATA_WIDTH  : integer  := 32;
+    -- Start count is the number of clock cycles the master will wait before initiating/issuing any transaction.
+    S2MM_COUNT  : integer  := 32
+  );
+  port (
+    aresetn, clock: in std_logic;
+    data_in: in std_logic_vector (31 downto 0);
+    time_in: in std_logic_vector (31 downto 0);
+    valid: in std_logic;
+    ready: out std_logic;
+    xrun_flag: out std_logic;
+    dma_trig, dma_reset: in std_logic;
+    dma_len: in unsigned (29 downto 0);
+    --
+    state_mon: out std_logic_vector (2 downto 0);
+    --
+    S2MM_tvalid : out std_logic;
+    S2MM_tready: in std_logic;
+    S2MM_tdata: out std_logic_vector (31 downto 0);
+    S2MM_tlast: out std_logic;
+    S2MM_tstrb: out std_logic_vector(3 downto 0)
+    );
+end zlogan_capt_rxdma;
+
+architecture rtl of zlogan_capt_rxdma is
+  type dma_state_t is (ST_WAIT_TRIG, ST_STAMP, ST_DATA);
+  signal state, nx_state: dma_state_t;
+  signal count, nx_count: unsigned (29 downto 0);
+  signal count_lo: unsigned (19 downto 0);
+  signal count_hi: unsigned (9 downto 0);
+  constant cnt_zeros: unsigned (count'range) := (others => '0');
+  constant cnt_ones: unsigned (count'range) := (others => '1');
+  constant cnt_lo_zeros: unsigned (count_lo'range) := (others => '0');
+  constant cnt_hi_zeros: unsigned (count_hi'range) := (others => '0');
+  signal ready_buf, xrun_flag_rg, dma_valid,
+    nx_xrun_flag, nx_dma_valid: std_logic;
+  signal dma_data, nx_dma_data: std_logic_vector (31 downto 0);
+begin
+  ready <= ready_buf;
+  xrun_flag <= xrun_flag_rg;
+  count_lo <= count(19 downto 0);
+  count_hi <= count(29 downto 20);
+  
+  buf: process (dma_data, dma_valid, dma_reset, xrun_flag_rg,
+                state, ready_buf, data_in, S2MM_tready)
+  begin
+    nx_dma_data <= dma_data;
+    nx_dma_valid <= dma_valid;
+    nx_xrun_flag <= xrun_flag_rg and not dma_reset;
+    if state = ST_DATA then
+      ready_buf <= not dma_valid;
+    else
+      ready_buf <= '0';
+    end if;
+    if valid = '1' then
+      nx_dma_data <= data_in;
+      nx_dma_valid <= '1';
+      if (ready_buf or S2MM_tready) = '0' then
+        nx_xrun_flag <= '1';
+      end if;
+    else
+      if (dma_valid = '1') and
+        (state = ST_DATA) and
+        (S2MM_tready = '1') then
+        nx_dma_valid <= '0';
+      end if;
+    end if;
+  end process;
+
+  fsm: process (state, count, count_lo, count_hi, dma_trig, dma_data, dma_len,
+                dma_valid, S2MM_tready)
+  begin
+    S2MM_tvalid <= '0';
+    S2MM_tlast <= '0';
+    nx_state <= state;
+    nx_count <= count;
+    S2MM_tdata <= dma_data;
+    S2MM_tstrb <= (others => '1');
+    --
+    case state is
+      when ST_WAIT_TRIG =>
+        state_mon <= "001";
+        if dma_trig = '1' then
+          nx_state <= ST_STAMP;
+          nx_count <= dma_len;
+        end if;
+
+      when ST_STAMP =>
+        state_mon <= "010";
+        S2MM_tvalid <= '1';
+        S2MM_tdata <= time_in;
+        if count_lo = cnt_lo_zeros then
+          S2MM_tlast <= '1';
+          if S2MM_tready = '1' then
+            nx_state <= ST_WAIT_TRIG;
+          end if;
+        else
+          if S2MM_tready = '1' then
+            nx_count <= count + cnt_ones;  -- count = count - 1
+            nx_state <= ST_DATA;
+          end if;
+        end if;
+
+      when ST_DATA =>
+        state_mon <= "100";
+        if dma_valid = '1' then
+          S2MM_tvalid <= '1';
+          if count_lo = cnt_lo_zeros then
+            S2MM_tlast <= '1';
+            if S2MM_tready = '1' then
+              nx_count <= count + cnt_ones;  -- count = count - 1
+              if count_hi = cnt_hi_zeros then
+                nx_state <= ST_WAIT_TRIG;
+              else
+                nx_state <= ST_STAMP;
+              end if;
+            end if;
+          else
+            if S2MM_tready = '1' then
+              nx_count <= count + cnt_ones;  -- count = count - 1
+            end if;
+          end if;
+        else
+          S2MM_tvalid <= '0';
+        end if;
+
+      when others =>
+    -- nihil
+    end case;
+  end process;
+
+  process
+  begin
+    wait until clock'event and clock = '1';
+    count <= nx_count;
+    dma_data <= nx_dma_data;
+    dma_valid <= nx_dma_valid;
+    xrun_flag_rg <= nx_xrun_flag;
+  end process;
+
+  process (aresetn, clock)
+  begin
+    if aresetn = '0' or dma_reset = '1' then
+      state <= ST_WAIT_TRIG;
+    elsif clock'event and clock = '1' then
+      state <= nx_state;
+    end if;
+  end process;
+end rtl;
